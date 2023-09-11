33dbd09b564cf6fdcfe97973428ed3e192592419
JAAR <joe.aaron.acosta@outlook.com>
2023-09-11 10:57:03 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 09/11/2023
+>>> Practicing Fundamentals Program 22
+>>> Version 0
+'''
+
+'''
+>>> Creates a program that asks the user to either create a new csv file or copy an existing csv file.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
